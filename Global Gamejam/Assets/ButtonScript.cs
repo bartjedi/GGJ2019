29a7a36959f4be7007742bd04e5d214a1d821707
Wasili Prattis <b.j.prattis@gmail.com>
2019-01-26 00:29:31 +0100

    ButtonSpawner

---
+++ Global Gamejam/Assets/ButtonScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+	[SerializeField]
+	private float bottomOfScreen = -10.0f;
+	[SerializeField]
+	private float fallingSpeed = 0.02f;
+
+	
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public virtual void Update()
+    {
+	
+		// Button is removed when not visible anymore
+		if(this.transform.position.y < bottomOfScreen)
+		{
+			Destroy(this.gameObject);
+		}
+		
+    }
+}
