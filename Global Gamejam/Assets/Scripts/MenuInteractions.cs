5255750d26ef7135d3107e556ed1abd6368c8046
ninjaxfish <marjoleinj94@hotmail.com>
2019-01-26 14:43:36 +0100

    Added buttonclick with outliner

---
+++ Global Gamejam/Assets/Scripts/MenuInteractions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuInteractions : MonoBehaviour
+{
+    // put the first material here.
+    public Material regularMaterial;
+    // put the second material here.
+    public Material ClickedMaterial;
+    public bool Clicked = false;
+    void Start()
+    {
+        Debug.Log("load YEAH");
+        GetComponent<Renderer>().material = regularMaterial;
+    }
+
+    void OnMouseDown()
+    {
+        Clicked = true;
+        GetComponent<Renderer>().material = ClickedMaterial;
+     
+    }
+    void OnMouseUp()
+    {
+        Clicked = false;
+        GetComponent<Renderer>().material = regularMaterial;
+
+    }
+}
