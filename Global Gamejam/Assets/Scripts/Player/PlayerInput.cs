a51589677569e0ea047163dce967307ae319be54
Bart Dikmans <bartjedi@hotmail.com>
2019-01-26 02:32:22 +0100

    Working plugin found for Xbox controllers. Works but requires minor tweaks in the playerinput

--- Global Gamejam/Assets/Scripts/Player/PlayerInput.cs
+++ Global Gamejam/Assets/Scripts/Player/PlayerInput.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XboxCtrlrInput;
 
 public class PlayerInput : MonoBehaviour
 {
+	public XboxController controller;
+
     public bool jump {
         get {
-            return Input.GetKeyDown(KeyCode.Space);
+			//return XCI.GetButtonDown(XboxButton.DOE HIER BUTTON A WASILI);
+			return false;
         }
     }
 
     public float horizontal {
         get {
-            return Input.GetAxisRaw("Horizontal");
+			return XCI.GetAxis(XboxAxis.LeftStickX, controller);
         }
     }
+
+	public float vertical {
+		get {
+			return XCI.GetAxis(XboxAxis.LeftStickY, controller);
+		}
+	}
 }
