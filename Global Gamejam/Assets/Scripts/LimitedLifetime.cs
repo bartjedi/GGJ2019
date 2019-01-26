3d1f95d871335d8efbc24fe754b08fefca569741
Bart Dikmans <bartjedi@hotmail.com>
2019-01-26 16:59:04 +0100

    omdat jij t bent

---
+++ Global Gamejam/Assets/Scripts/LimitedLifetime.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimitedLifetime : MonoBehaviour
+{
+    [SerializeField]
+    private float timeToLive = 10f;
+    private float startTime;
+
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startTime + timeToLive < Time.time)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
