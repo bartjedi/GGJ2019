606ddb280374d8bf020fbba0862921fe1dbb4bcf
CoreBank-L-09 <you@example.com>
2019-01-27 00:53:03 +0100

    FinishScene

---
+++ Global Gamejam/Assets/Scripts/EndScreenButtonEffects.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScreenButtonEffects : MonoBehaviour
+{
+    public Material[] materials;
+
+    private void Awake ()
+    {
+        Vector3 rot = new Vector3(Random.value * 360f, Random.value * 360f, Random.value * 360f);
+        transform.eulerAngles = rot;
+
+        if (materials.Length > 0)
+        {
+            int random = Random.Range(0, materials.Length);
+            Debug.Log(random);
+            GetComponent<MeshRenderer>().material = materials[random];
+        }
+    }
+}
