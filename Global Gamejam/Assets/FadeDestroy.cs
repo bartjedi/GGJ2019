31560839640d2ac315f309f26ea0ec20daadfb03
Wasili Prattis <b.j.prattis@gmail.com>
2019-01-27 14:29:32 +0100

    fade

---
+++ Global Gamejam/Assets/FadeDestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeDestroy : MonoBehaviour
+{
+	Image img;
+
+	IEnumerator Start()
+	{
+		Color c = img.color;
+		while (c.a > 0.0f)
+		{
+			c.a -= Time.deltaTime;
+			img.color = c;
+			yield return new WaitForEndOfFrame();
+		}
+		Destroy(this.gameObject);
+	}
+}
